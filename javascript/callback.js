98ce9e3c352e6dddc4cd5075189a6468337788df
igoryen <igor.yentaltsev@gmail.com>
2016-12-15 13:23:04 -0500

    callback.js: created

---
+++ javascript/callback.js
@@ -0,0 +1,10 @@
+var friends = ["Mike", "Stacy", "Andy", "Rick"];
+
+// 1
+friends.forEach( function ( friend, index ){
+	console.log(index + 1 + ". " + friend ); // 1. Mike, 2. Stacy, 3. Andy, 4. Rick​
+});
+
+/*
+1. iterate through the VALUES of the elements of the array.
+*/
