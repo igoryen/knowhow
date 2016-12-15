d82282f059bac6cc9888ca38f85e5fe88a59773f
igoryen <igor.yentaltsev@gmail.com>
2016-12-15 12:44:43 -0500

    accessor-props.js: added 'random'

--- javascript/accessor-props.js
+++ javascript/accessor-props.js
@@ -53,13 +53,33 @@ console.log(
 	s.next = 11, 
 	s.next = 14
 );
-console.log( 
-	s.next = 4
-);
+// 10
+// console.log( 
+// 	s.next = 4
+// );
 //======================================================
 
+var random = {
+	// 8
+	get octet() {
+		return Math.floor( Math.random() * 256 ); // 9
+	},
+	get uint16() {
+		return Math.floor( Math.random() * 65536 );
+	},
+	get int16() {
+		return Math.floor( Math.random() * 65536 ) - 32768;
+	}
+};
 
 
+var rand = Object.create( random );
+
+console.log(
+	rand.octet,
+	rand.uint16,
+	rand.int16
+);
 
 /*
 1. a regular read-write data property
@@ -70,4 +90,8 @@ console.log(
 	The name `$n` (PHP style, with a dollar sign) hints that it's a provate property
 6. return the current value and then increment it.
 7. the accessor property is a PROPERTY, so you don't pass a value to it, but use `=`.
+8. Accessor properties. They return random numbers.
+9. yield a random number between 0 and 255 each time octet() is evaluated.
+10. Commented out because it demonstrates an error and the execution doesn't go further from there.
+
 */
