146c1cef569b4c8286abe9ed4d3d485878c31367
igoryen <igor.yentaltsev@gmail.com>
2016-12-15 17:41:52 -0500

    callbacks3.js: augmented

---
+++ javascript/callback3.js
@@ -0,0 +1,56 @@
+// 2
+function getUserInput( firstName, lastName, gender, callback ) {
+    var fullName = firstName + " " + lastName;
+
+    if ( typeof callback === "function" ) { // 3
+	    callback( fullName, gender ); // 4
+    }
+}
+//...................................................
+// 1
+function genericPoemMaker( name, gender ) {
+
+	var line = function ( n ) {
+		var strike = ".";
+		for (var i = 0; i < n; i++) {
+			strike += ".";
+		}
+		return strike;
+	};
+	
+	console.log(line(30));
+    console.log(name + " is finer than fine wine.");
+    console.log("Altruistic and noble for the modern time.");
+    console.log("Always admirably adorned with the latest style.");
+    console.log("A " + gender + " of unfortunate tragedies who still manages a perpetual smile");
+	console.log(line(30));
+}
+
+function greetUser ( customerName, sex ) {
+	var salutation = sex && sex == "Man" ? "Mr." : "Ms.";
+	console.log( "Hello, " + salutation + " " + customerName + "!" );
+}
+
+//....................................................
+
+var p = {	
+	fname: "Alan",
+	lname: "Rickman",
+	gender: "Man"	
+}
+
+getUserInput( p.fname, p.lname, p.gender, genericPoemMaker ); // 5
+getUserInput( p.fname, p.lname, p.gender, greetUser );
+
+
+/*
+
+1. First, setup the generic poem creator function; it will be the callback function in the getUserInput function below.
+2. The callback, which is parameter D, will be our genericPoemMaker function we defined above.
+3. Make sure the callback is a function i.e. callable
+4. Execute the callback function and pass the parameters to it
+5. Because the getUserInput function is only handling the retrieving of data, 
+	we can pass any callback to it. 
+
+*/
+
