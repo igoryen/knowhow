30826141b90e3918814f0f1a06657be02f62655e
igoryen <igor.yentaltsev@gmail.com>
2016-12-15 15:52:37 -0500

    callback2.js: augmented

--- javascript/callback2.js
+++ javascript/callback2.js
@@ -1,10 +1,10 @@
-allUserData = []; // 1
+var allUserData = []; // 1
 // 2 
 function logStuff ( userData ) {
-    if ( typeof userData === "string") {
+    if ( typeof userData === "string") { // 10
         console.log(userData);
     }
-    else if ( typeof userData === "object") {
+    else if ( typeof userData === "object") { // 9
         for (var item in userData) {
             console.log(item + ": " + userData[item]);
         }
@@ -14,20 +14,46 @@ function logStuff ( userData ) {
 // 3
 function getInput (options, callback) {
     allUserData.push (options);
-    callback (options);
+    callback (options); // 7
 }
 
 var user = { name:"Rich", speciality:"JavaScript" };
 
 // 4
-getInput ( user, logStuff);
+getInput ( user, logStuff );
+
+// ......................................................
+var generalLastName = "Smith"; // 8
+
+
+function getInput2 (options, callback) {
+    allUserData.push (options);
+    if( typeof callback === "function" ) { // 6
+        callback ( generalLastName, options ); // 5
+    }
+}
+
+getInput2( user, logStuff );
+
+//
+// allUserData.forEach( function( user ) {
+//     console.log( user );
+// });
 
 /*
-1. global variable
+1. global variable of type Array object
 2. generic logStuff function. It prints to console.
 3. A function. It takes two parameters, 
     parameter A: an object.
     parameter B: a callback function
 4. When we call the `getInput` function, we pass `logStuff` as a parameter.
     So `logStuff` will be the function that will called back (or executed) inside the `getInput` function
+5. Passing 2 parameters to the callback:
+    the global variable to the callback
+6. Make sure that the callback is callable, i.e. a function
+7. Passing just one parameter to the callback:
+    the `options` - which can be an object or a String
+8. global variable of type String
+9. like, e.g. an Array (#1)
+10. like, e.g. a String (#8)
 */
