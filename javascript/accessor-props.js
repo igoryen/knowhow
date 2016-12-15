b9bb05f85776caa6a3599b1e77dfeb86591d5459
igoryen <igor.yentaltsev@gmail.com>
2016-12-15 12:33:05 -0500

    accessor-props.js: augmented

---
+++ javascript/accessor-props.js
@@ -0,0 +1,73 @@
+var p = {
+	x: 1.0, // 1
+	y: 1.0, // 1
+
+	// 2
+	get r() {
+		return Math.sqrt( this.x * this.x + this.y * this.y );
+	},
+	set r( newvalue ) {
+		var oldvalue = this.x * this.x + this.y * this.y;
+		var ratio = newvalue / oldvalue;
+		
+		this.x *= ratio;
+		this.y *= ratio; 
+	},
+	// 3
+	get theta() {
+		return Math.atan2( this.y, this.x );
+	}
+};
+
+var q = Object.create( p );
+q.x = 1;
+q.y = 1;
+
+console.log( q.r );
+console.log( q.theta );
+
+// =====================================================
+// 4
+var serialnum = {
+	$n: 0, // 5
+	get next() {
+		return this.$next++; // 6
+	},
+	set next( n ) {
+		if ( n > this.$n ) {
+			this.$n = n;
+		}
+		else {
+			throw "Serial number can only be set to a larger value (than " + this.$n + ")";
+		}
+	}
+};
+
+var s = Object.create( serialnum );
+// 7
+console.log( 
+	s.next = 6, 
+	s.next = 7,
+	s.next = 8, 
+	s.next = 9, 
+	s.next = 11, 
+	s.next = 14
+);
+console.log( 
+	s.next = 4
+);
+//======================================================
+
+
+
+
+/*
+1. a regular read-write data property
+2. `r()` a read-and-write accessor property with getter and setter
+3. `theta()` a read-only accessor property with getter only
+4. This object generates strictly increasing serial numbers
+5. a data property. Holds the next serial number. 
+	The name `$n` (PHP style, with a dollar sign) hints that it's a provate property
+6. return the current value and then increment it.
+7. the accessor property is a PROPERTY, so you don't pass a value to it, but use `=`.
+*/
