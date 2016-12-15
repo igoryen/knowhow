29d00d31926e51159aeea20eef22086ad908a087
igoryen <igor.yentaltsev@gmail.com>
2016-12-15 16:34:05 -0500

    callback2.js: augmented with clientData

--- javascript/callback2.js
+++ javascript/callback2.js
@@ -35,10 +35,30 @@ function getInput2 (options, callback) {
 
 getInput2( user, logStuff );
 
-//
-// allUserData.forEach( function( user ) {
-//     console.log( user );
-// });
+//.......................................................
+
+var clientData = {
+    id: 094545,
+    phone: "111-222-333-444",
+    fullName: "Not Set",
+    setUserName: function (firstName, lastName)  {  // 14
+      this.fullName = firstName + " " + lastName;
+    }
+}
+
+function getUserInput(firstName, lastName, callback, callbackObj)  {
+    // 11
+    callback.apply ( callbackObj, [firstName, lastName] ); // 12
+}
+
+getUserInput("Willie", "Wonka", clientData.setUserName, clientData); // 15
+
+console.log( clientData.fullName, clientData.id, clientData.phone );
+
+// console.log( window.fullName ); // 13
+
+//.............................................................................
+
 
 /*
 1. global variable of type Array object
@@ -56,4 +76,19 @@ getInput2( user, logStuff );
 8. global variable of type String
 9. like, e.g. an Array (#1)
 10. like, e.g. a String (#8)
+11. Do other stuff to validate firstName/lastName here
+12. Saving the names.
+    every JS function has method `apply()`.
+    Use `apply()` method to set `this` to the `callbackObj`.
+    Narrative: 
+    `this` gets the callbackObj, 
+    pass the values in the array to the newly-set `this`
+13. 
+14. when clientData.setUserName is executed, 
+    this.fullName will not set the fullName property on the clientData object. 
+    Instead, it will set fullName on the global object (the window object in the browser), 
+    since getUserInput is a global function (a function on the global object). 
+    This happens because the `this` object in the global function points to the window object.
+15. Parameter D: the callback object `clientData`
+
 */
